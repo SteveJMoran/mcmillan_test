eca4c463e559aa87b94a97a2837d8cb4c5e86ebd
Steve Moran <stevejamesmoran@gmail.com>
2015-10-07 20:07:21 -0400

    adds slick

--- js/script.js
+++ js/script.js
@@ -11,4 +11,37 @@ $(function() {
       }
     }
   });
+
+
+  $('.carousel').slick({
+    dots: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1080,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 665,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 });
